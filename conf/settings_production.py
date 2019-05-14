3db91560ed5ec6ad011636e22754586dbf593468
jiaquan <jiaquan@canway.net>
2019-05-14 16:38:43 +0800

    helloworld

--- conf/settings_production.py
+++ conf/settings_production.py
@@ -11,10 +11,10 @@ from settings import APP_ID
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
-        'NAME': APP_ID,                        # 数据库名 (默认与APP_ID相同)
-        'USER': '',                            # 你的数据库user
-        'PASSWORD': '',                        # 你的数据库password
-        'HOST': '',                   		   # 数据库HOST
+          'NAME': 'app_id',                        # 数据库名 (默认与APP_ID相同)
+        'USER': 'root',                        # 你的数据库user
+        'PASSWORD': '123',                        # 你的数据库password
+        'HOST': '192.168.100.131',                   # 开发的时候，使用localhost
         'PORT': '3306',                        # 默认3306
     },
 }
