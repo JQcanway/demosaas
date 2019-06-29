cfd90b4a7bc42c2f1628d01f47f9ff9f26b43678
jiaquan <jiaquan@canway.net>
2019-06-29 13:30:13 +0800

    考试2

--- conf/settings_production.py
+++ conf/settings_production.py
@@ -13,8 +13,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
           'NAME': 'jiaquan',                        # 数据库名 (默认与APP_ID相同)
         'USER': 'root',                        # 你的数据库user
-        'PASSWORD': 'root',                        # 你的数据库password
-        'HOST': '192.168.200.18',                   # 开发的时候，使用localhost
+          'PASSWORD': 'YtX#_]O[6n',  # 你的数据库password
+        'HOST': '192.168.163.58',  # 开发的时候，使用localhost
         'PORT': '3306',                        # 默认3306
     },
 }
